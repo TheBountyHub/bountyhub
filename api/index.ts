f6fca49db1a88eb2b9ca09ee96c846ff927b44aa
Lionel Lewis <lionellewis815@gmail.com>
2021-10-18 13:26:22 -0500

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(cookieParser());
+
+// Kubernetes health checking
+app.get('/', (req, res) => res.send('API is healthy'));
+
+app.get('/github', async (req, res) => {});
+app.get('/hackerone', async (req, res) => {});
+app.get('/bugcrowd', async (req, res) => {});
+app.post('/refresh', async (req, res) => {});
+app.post('/logout', (req, res) => {});
+app.post('/logout-all', async (req, res) => {});
+app.get('/me', async (req, res) => {});
+
+// Run Server
+app.listen(3000, () => {
+  console.log('We are The BountyHub');
+});
