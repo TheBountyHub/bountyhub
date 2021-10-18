d3b956d41e97d1d7de1b8806e6935997c8db7ace
Lionel Lewis <lionellewis815@gmail.com>
2021-10-18 13:54:22 -0500

    Create github-adapter.ts

---
+++ api/helpers/github-adapter.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+require('dotenv').config({
+  path: '../../config/.env',
+});
+
+interface GitHubUser {
+  id: number;
+  name: string;
+}
+
+interface AccessTokenResponse {
+  access_token: string;
+}
+
+interface UserResponse {
+  id: number;
+  name: string;
+}
+
+const TOKEN_URL = 'https://github.com/login/oauth/access_token';
+const USER_URL = 'https://api.github.com/user';
+
+export async function getGitHubUser(code: string) {
+  const token = await getAccessToken(code);
+  return getUser(token);
+}
+
+async function getAccessToken(code: string) {
+  const response = await axios.post<AccessTokenResponse>(
+    TOKEN_URL,
+    {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    },
+    {
+      headers: {Accept: 'application/json'},
+    }
+  );
+
+  return response.data.access_token;
+}
